5699ef90779b29506027d680dd9f3ffb2de146f3
unknown <nguyenhoangkhang12102003@gmail.com>
2023-10-03 15:29:04 +0700

    add admin and shop routes

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const adminRouter = require("./routes/admin")
+const shopRouter = require("./routes/shop");
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/admin", adminRouter);
+app.use(shopRouter);
 
-app.use("/", (req, res, next) => {
-    next();
-})
-app.get("/add-product", (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-})
-
-app.post("/product", (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use("/", (req, res, next) => {
-    res.send("<h1>Hello from Express!</h1>");
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page not found</h1>");
 })
 
 
