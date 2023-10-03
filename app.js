12b5bd66840b84a1048ef59767e2c5abfe178533
unknown <nguyenhoangkhang12102003@gmail.com>
2023-10-03 16:04:47 +0700

    add html for shop and admin page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 const adminRouter = require("./routes/admin")
 const shopRouter = require("./routes/shop");
 
@@ -10,8 +11,8 @@ app.use("/admin", adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found</h1>");
-})
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res) => {
