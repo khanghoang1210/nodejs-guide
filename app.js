05c441125dbe08484206c5fc42043292c58e3e5b
unknown <nguyenhoangkhang12102003@gmail.com>
2023-10-05 18:28:05 +0700

    add layouts

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(shopRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+    res.status(404).render("404", { pageTitle: "Page not found" });
 });
 
 
