fba55e5e22a5142b19941235018f1d6e27a169ff
unknown <nguyenhoangkhang12102003@gmail.com>
2023-10-06 11:09:24 +0700

    refactor models

--- models/product.js
+++ models/product.js
@@ -4,11 +4,20 @@ const e = require("express");
 const fs = require("fs");
 const path = require("path");
 const products = [];
+
 const p = path.join(
     path.dirname(require.main.filename),
     'data',
     'products.json');
 
+const getProductsFromFile = callback => {
+    fs.readFile(p, (error, fileContent) => {
+        if (error) {
+            callback([]);
+        }
+        callback(JSON.parse(fileContent));
+    });
+}
 module.exports = class Product {
 
     constructor(title) {
@@ -16,26 +25,15 @@ module.exports = class Product {
     };
 
     save() {
-
-        fs.readFile(p, (error, fileContent) => {
-            let products = [];
-            if (!error) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (error) => {
                 console.log(error);
             })
         })
-
     }
 
     static fetAll(callback) {
-        fs.readFile(p, (error, fileContent) => {
-            if (error) {
-                callback([]);
-            }
-            callback(JSON.parse(fileContent));
-        });
+        getProductsFromFile(callback)
     }
 };
