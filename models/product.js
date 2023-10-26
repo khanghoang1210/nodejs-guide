b297e83089cdd3a4c2137085624bbffb9d0a00ea
unknown <nguyenhoangkhang12102003@gmail.com>
2023-10-26 22:26:57 +0700

    fetch single product by mongodb

--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 // const Sequelize = require('sequelize');
 
 // const sequelize = require("../util/database");
-
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -31,6 +31,18 @@ class Product {
                 return products
             })
             .catch(err => console.log(err));
+    }
+
+    static findByPk(prodId) {
+        const db = getDb();
+        return db.collection("products")
+            .find({ _id: new mongodb.ObjectId(prodId) })
+            .next()
+            .then(product => {
+                console.log(product);
+                return product;
+            })
+            .catch(err => console.log(err))
     }
 }
 
